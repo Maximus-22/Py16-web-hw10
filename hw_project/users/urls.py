cd8db851f9b006fce750e0d866d04a72c09c07d8
Maximus-22 <maksim.melnichenko@nservice.com.ua>
2023-11-28 00:42:41 +0200

    Module auth added

--- hw_project/users/urls.py
+++ hw_project/users/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
+from .forms import LoginForm
 
 
-app_name = 'quotes'
+app_name = 'users'
 
 urlpatterns = [
-    path('', views.main, name = 'root'),
+    path('signup/', views.RegisterView.as_view(), name = 'signup'),
+    path('login/', LoginView.as_view(template_name = "users/login.html", form_class = LoginForm), name = 'login'),
+    path('logout/', LogoutView.as_view(template_name = "users/logout.html"), name = 'logout'),
 ]
